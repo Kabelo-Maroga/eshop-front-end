a8bc64bfac1d8bf8dfb42897d7de85e9ede846d6
Kabelo Maroga <kabelo@psybergate.co.za>
2021-03-18 17:17:15 +0200

    installing bootstrap & firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAo4gzCqZ8dWsQhaaOIkuxMb4sTKylWI34',
+    authDomain: 'e-shop-5ead0.firebaseapp.com',
+    projectId: 'e-shop-5ead0',
+    storageBucket: 'e-shop-5ead0.appspot.com',
+    messagingSenderId: '579937751632',
+    appId: '1:579937751632:web:39f5d666319c7cbea4b755',
+    measurementId: 'G-PNJRMP155N'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAo4gzCqZ8dWsQhaaOIkuxMb4sTKylWI34',
+    authDomain: 'e-shop-5ead0.firebaseapp.com',
+    projectId: 'e-shop-5ead0',
+    storageBucket: 'e-shop-5ead0.appspot.com',
+    messagingSenderId: '579937751632',
+    appId: '1:579937751632:web:39f5d666319c7cbea4b755',
+    measurementId: 'G-PNJRMP155N'
+  }
 };
 
 /*
