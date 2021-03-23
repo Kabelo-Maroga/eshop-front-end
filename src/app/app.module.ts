6342afc74f18883438a94f13c4b17d8d1208f025
Kabelo Maroga <kabelo@psybergate.co.za>
2021-03-23 23:20:38 +0200

    added validations to the product form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
 import {CategoryService} from './services/category.service';
 import {FormsModule} from '@angular/forms';
 import {ProductService} from './services/product.service';
+import {CustomFormsModule} from 'ng2-validation';
 
 @NgModule({
   declarations: [
@@ -49,6 +50,7 @@ import {ProductService} from './services/product.service';
   ],
   imports: [
     BrowserModule,
+    CustomFormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
