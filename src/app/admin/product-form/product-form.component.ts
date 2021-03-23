084b0298c28732bb5dd8f9e6abb8041b3ef3b0bd
Kabelo Maroga <kabelo@psybergate.co.za>
2021-03-23 23:38:22 +0200

    added a card to preview product as it is created.

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {CategoryService} from '../../services/category.service';
 import {Observable} from 'rxjs';
 import {ProductService} from '../../services/product.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -12,11 +13,14 @@ export class ProductFormComponent {
 
   categories: Observable<any[]>;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) {
+  constructor(private categoryService: CategoryService,
+              private productService: ProductService,
+              private router: Router) {
     this.categories = categoryService.getCategories();
   }
 
   save(product: any) {
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 }
