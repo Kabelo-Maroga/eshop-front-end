3486339193cebb98e1920d9577d083364f1146d3
Kabelo Maroga <kabelo@psybergate.co.za>
2021-03-24 00:08:23 +0200

    added a table to displayed saved products.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {ProductService} from '../../services/product.service';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.scss']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent {
 
-  constructor() { }
+  products: Observable<any[]>;
 
-  ngOnInit(): void {
+  constructor(private productService: ProductService) {
+    this.products = productService.getAll();
   }
-
 }
